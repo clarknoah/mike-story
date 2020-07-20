ecdde1d1bc8cc9ac52b7454caf322c29e7434b8f
Noah Clark <noahbc08@gmail.com>
2020-07-20 14:37:53 -0400

    added code

---
+++ precache-manifest.93819d3b27cb9858f6a149ada66dc6b1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e9d0ee2d6b095a71b303f3836ccb3c2",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.f861006699de9322f645.bundle.js"
+  },
+  {
+    "url": "runtime~main.f861006699de9322f645.bundle.js"
+  },
+  {
+    "revision": "1d13292128414c64872d",
+    "url": "static/css/main.65a64597.chunk.css"
+  },
+  {
+    "revision": "efbd242f8a3b059a201289bcee975ab5",
+    "url": "static/media/beats.efbd242f.svg"
+  },
+  {
+    "revision": "f061aae4ac0a7b27111d0fc93ee9bfc6",
+    "url": "static/media/cart.f061aae4.svg"
+  },
+  {
+    "revision": "9801357361ca2953aecaf9e378de9525",
+    "url": "static/media/cart_white.98013573.svg"
+  },
+  {
+    "revision": "0ae49bb896d2b79458854626af2d38db",
+    "url": "static/media/check.0ae49bb8.svg"
+  },
+  {
+    "revision": "fc84bcac94ada84fc3a39b2219b45bd5",
+    "url": "static/media/grey_star.fc84bcac.svg"
+  },
+  {
+    "revision": "bcf238ce2e933551b97469b6a96ef901",
+    "url": "static/media/heart-white.bcf238ce.svg"
+  },
+  {
+    "revision": "cce5b434e5d3292b940b8f39bcb56216",
+    "url": "static/media/hearts.cce5b434.svg"
+  },
+  {
+    "revision": "d08b1ddfed66b9b3ad62e69facce6e61",
+    "url": "static/media/hot.d08b1ddf.svg"
+  },
+  {
+    "revision": "053b7fcaee349d0cb42531ea93bd4e3f",
+    "url": "static/media/macbook_pro.053b7fca.svg"
+  },
+  {
+    "revision": "1a80d912375498d3539cd515d8921c52",
+    "url": "static/media/select_arrow.1a80d912.svg"
+  },
+  {
+    "revision": "f8ce72af7cb60bed6f65146e89c9d459",
+    "url": "static/media/yellow_star.f8ce72af.svg"
+  },
+  {
+    "url": "vendors~main.f861006699de9322f645.bundle.js"
+  },
+  {
+    "url": "vendors~main.f861006699de9322f645.bundle.js.LICENSE.txt"
+  }
+]);
